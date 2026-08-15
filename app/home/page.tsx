732e789d1de0b328575292cbd0142443db24e41e
Danish Khan <danishkhan4126@gmail.com>
2026-08-15 21:57:31 +0530

    🔧 Hotfix: Removed ghost onSelectMedia prop from HomePage to clear TypeScript build error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,10 @@
-"use client";
-
 import React from "react";
 import HomeView from "@/components/ui/home-view";
-import { useModal } from "@/context/ModalContext";
 
 export default function HomePage() {
-  const { setSelectedMedia } = useModal();
-
   return (
     <div style={{ width: "100%", minHeight: "100%" }}>
-      <HomeView onSelectMedia={setSelectedMedia} />
+      <HomeView />
     </div>
   );
 }
