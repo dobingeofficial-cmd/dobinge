efceeaffb44b5da92126ddce38a452cc86664a7f
Danish Khan <danishkhan4126@gmail.com>
2026-08-25 23:45:39 +0530

    Continue

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -9,7 +9,7 @@ export default function DiscoverPage() {
 
   return (
     // 🚨 HARD FIX: Bottom padding is now 0px so the grid bleeds all the way to the bottom edge of the screen.
-    <div style={{ width: "100%", height: "calc(100vh - 80px)", padding: "24px 32px 0px 32px", boxSizing: "border-box", overflow: "hidden" }}>
+    <div style={{ width: "100%", height: "calc(100vh - 40px)", padding: "24px 32px 0px 32px", boxSizing: "border-box", overflow: "hidden" }}>
       <DiscoverView onSelectMedia={setSelectedMedia} />
       
       <MediaModal 
